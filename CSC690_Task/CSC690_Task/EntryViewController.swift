2a4f4727d74d037b58fe773bf2d2d816e1bb2eda
Mai Ra <mra1@mail.sfsu.edu>
2021-12-09 21:32:52 -0800

    first

---
+++ CSC690_Task/CSC690_Task/EntryViewController.swift
@@ -0,0 +1,19 @@
+//
+//  EntryViewController.swift
+//  CSC690_Task
+//
+//  Created by Mai Ra on 12/9/21.
+//
+
+import UIKit
+
+class EntryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
